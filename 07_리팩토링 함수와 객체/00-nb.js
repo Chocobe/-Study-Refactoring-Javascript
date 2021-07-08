04ea33be730bc8b78e8af7ec8520826359fde611
김영우 <kyw05171@gmail.com>
2021-07-09 00:35:50 +0900

    <김영우>: 00-nb.js에서 classifier객체를 추출 하였습니다.

--- 07_리팩토링 함수와 객체/00-nb.js
+++ 07_리팩토링 함수와 객체/00-nb.js
@@ -1,110 +1,129 @@
 // NBC (Naive Bayes Classifier)
+var classifier = {
+  // 셋업 함수
+  songs: [],
+  allChords: new Set(),
+  labelCounts: new Map(),
+  labelProbabilities: new Map(),
+  chordCountsInLabels: new Map(),
+  probabilityOfChordsInLabels: new Map(),
+};
 
 // 현재 파일명 추출 함수
 function fileName() {
   const theError = new Error("here I am");
-  return theError.stack.match(/(\w+\.js)/)[1];
+  return theError.stack.match(/(\d+-?\w+\.js)/)[1];
 }
 
-console.log(`Welcome to ${fileName()}!`);
+// 환영 메시지 함수
+function welcomeMessage() {
+  return `Welcome to ${fileName()}!`;
+}
 
-const easy = "easy";
-const medium = "medium";
-const hard = "hard";
+// 난이도 종류 설정 함수
+function setDifficulties() {
+  easy = "easy";
+  medium = "medium";
+  hard = "hard";
+}
 
-// 노래별 구성 코드
-imagine = ["c", "cmaj7", "f", "am", "dm", "g", "e7"];
-somewhereOverTheRainbow = ["c", "em", "f", "g", "am"];
-tooManyCooks = ["c", "g", "f"];
-iWillFollowYouIntoTheDark = ["f", "dm", "bb", "c", "a", "bbm"];
-babyOneMoreTime = ["cm", "g", "bb", "eb", "fm", "ab"];
-creep = ["g", "gsus4", "c", "cmsus4", "cm6"];
-paperBag = ["bm7", "e", "c", "g", "b7", "f", "em", "a", "cmaj7", "em7", "a7", "f7", "b"];
-toxic = ["cm", "eb", "g", "cdim", "eb7", "d7", "db7", "ab", "gmaj7", "g7"];
-bulletproof = ["d#m", "g#", "b", "f#", "g#m", "c#"];
-
-let songs = [];
-let allChords = new Set();
-let labelCounts = new Map();
-let labelProbabilities = new Map();
-let chordCountsInLabels = new Map();
-let probabilityOfChordsInLabels = new Map();
+// 노래 설정 함수
+function setSongs() {
+  imagine = ["c", "cmaj7", "f", "am", "dm", "g", "e7"];
+  somewhereOverTheRainbow = ["c", "em", "f", "g", "am"];
+  tooManyCooks = ["c", "g", "f"];
+  iWillFollowYouIntoTheDark = ["f", "dm", "bb", "c", "a", "bbm"];
+  babyOneMoreTime = ["cm", "g", "bb", "eb", "fm", "ab"];
+  creep = ["g", "gsus4", "c", "cmsus4", "cm6"];
+  paperBag = ["bm7", "e", "c", "g", "b7", "f", "em", "a", "cmaj7", "em7", "a7", "f7", "b"];
+  toxic = ["cm", "eb", "g", "cdim", "eb7", "d7", "db7", "ab", "gmaj7", "g7"];
+  bulletproof = ["d#m", "g#", "b", "f#", "g#m", "c#"];
+}
 
 function train(chords, label) {
-  songs.push({
+  classifier.songs.push({
     label,
     chords,
   });
   
-  chords.forEach(chord => allChords.add(chord));
+  chords.forEach(chord => classifier.allChords.add(chord));
 
-  if(Array.from(labelCounts.keys()).includes(label)) {
-    labelCounts.set(label, labelCounts.get(label) + 1);
+  if(Array.from(classifier.labelCounts.keys()).includes(label)) {
+    classifier.labelCounts.set(label, classifier.labelCounts.get(label) + 1);
   } else {
-    labelCounts.set(label, 1);
+    classifier.labelCounts.set(label, 1);
   }
 }
 
 function setLabelProbabilities() {
-  labelCounts.forEach((_count, label) => {
-    labelProbabilities.set(label, labelCounts.get(label) / songs.length);
+  classifier.labelCounts.forEach((_count, label) => {
+    classifier.labelProbabilities.set(label, classifier.labelCounts.get(label) / classifier.songs.length);
   });
 }
 
 function setChordCountsInLabels() {
-  songs.forEach(song => {
-    if(chordCountsInLabels.get(song.label) === undefined) {
-      chordCountsInLabels.set(song.label, {});
+  classifier.songs.forEach(song => {
+    if(classifier.chordCountsInLabels.get(song.label) === undefined) {
+      classifier.chordCountsInLabels.set(song.label, {});
     }
 
     song.chords.forEach(chord => {
-      if(chordCountsInLabels.get(song.label)[chord] > 0) {
-        chordCountsInLabels.get(song.label)[chord] += 1;
+      if(classifier.chordCountsInLabels.get(song.label)[chord] > 0) {
+        classifier.chordCountsInLabels.get(song.label)[chord] += 1;
       } else {
-        chordCountsInLabels.get(song.label)[chord] = 1;
+        classifier.chordCountsInLabels.get(song.label)[chord] = 1;
       }
     });
   });
 }
 
 function setProbabilityOfChordsInLabels() {
-  probabilityOfChordsInLabels = chordCountsInLabels;
+  classifier.probabilityOfChordsInLabels = classifier.chordCountsInLabels;
 
-  probabilityOfChordsInLabels.forEach((_chords, difficulty) => {
-    Object.keys(probabilityOfChordsInLabels.get(difficulty)).forEach(chord => {
-      probabilityOfChordsInLabels.get(difficulty)[chord] /= songs.length;
+  classifier.probabilityOfChordsInLabels.forEach((_chords, difficulty) => {
+    Object.keys(classifier.probabilityOfChordsInLabels.get(difficulty)).forEach(chord => {
+      classifier.probabilityOfChordsInLabels.get(difficulty)[chord] /= classifier.songs.length;
     });
   });
 }
 
-train(imagine, easy);
-train(somewhereOverTheRainbow, easy);
-train(tooManyCooks, easy);
+// 레이블과 확률 설정 함수
+function setLabelAndProbabilities() {
+  setLabelProbabilities();
+  setChordCountsInLabels();
+  setProbabilityOfChordsInLabels();
+}
 
-train(iWillFollowYouIntoTheDark, medium);
-train(babyOneMoreTime, medium);
-train(creep, medium);
+// 머신러닝 훈련 통합 함수
+function trainAll() {
+  setDifficulties();
+  setSongs();
+  
+  train(imagine, easy);
+  train(somewhereOverTheRainbow, easy);
+  train(tooManyCooks, easy);
+  
+  train(iWillFollowYouIntoTheDark, medium);
+  train(babyOneMoreTime, medium);
+  train(creep, medium);
+  
+  train(paperBag, hard);
+  train(toxic, hard);
+  train(bulletproof, hard);
 
-train(paperBag, hard);
-train(toxic, hard);
-train(bulletproof, hard);
+  setLabelAndProbabilities();
+}
 
-setLabelProbabilities();
-setChordCountsInLabels();
-setProbabilityOfChordsInLabels();
-
-function classify2(chords) {
+function classify(chords) {
   const smoothing = 1.01;
-
-  console.log(labelProbabilities);
 
   const classified = new Map();
 
-  labelProbabilities.forEach((_probabilities, difficulty) => {
-    let first = labelProbabilities.get(difficulty) + smoothing;
+  classifier.labelProbabilities.forEach((_probabilities, difficulty) => {
+    let first = classifier.labelProbabilities.get(difficulty) + smoothing;
 
     chords.forEach(chord => {
-      let probabilityOfChordInLabel = probabilityOfChordsInLabels.get(difficulty)[chord];
+      let probabilityOfChordInLabel = classifier.probabilityOfChordsInLabels.get(difficulty)[chord];
 
       if(probabilityOfChordInLabel) {
         first *= (probabilityOfChordInLabel + smoothing);
@@ -115,8 +134,32 @@ function classify2(chords) {
     classified.set(difficulty, first);
   });
 
-  console.log(classified);
+  return classified;
 }
 
-classify2(["d", "g", "e", "dm"]);
-classify2(["f#m7", "a", "dadd9", "dmaj7", "bm", "bm7", "d", "f#m"]);
+const wish = require("wish");
+
+describe("the file", () => {
+  trainAll();
+  
+  // classify() 함수의 "특성화 테스트"
+  it("classifies", () => {
+    const classified = classify(["f#m7", "a", "dadd9", "dmaj7", "bm", "bm7", "d", "f#m"]);
+
+    wish(classified.get(easy) === 1.3433333333333333);
+    wish(classified.get(medium) === 1.5060259259259259);
+    wish(classified.get(hard) === 1.6884223991769547);
+  });
+
+  // 환영 메시지 테스트
+  it("welcomeMessage()", () => {
+    wish(welcomeMessage() === "Welcome to 00-nb.js!");
+  });
+
+  // labelProbabilities 특성화 테스트
+  it("label probabilities", () => {
+    wish(classifier.labelProbabilities.get(easy) === 0.3333333333333333);
+    wish(classifier.labelProbabilities.get(medium) === 0.3333333333333333);
+    wish(classifier.labelProbabilities.get(hard) === 0.3333333333333333);
+  })
+});
