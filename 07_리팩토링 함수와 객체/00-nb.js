c3c4e4a4edca4f719917be0ddfea84dffd922917
김영우 <kyw05171@gmail.com>
2021-07-09 15:01:15 +0900

    <김영우>: 전역변수를 객체로 추출 하였습니다.

--- 07_리팩토링 함수와 객체/00-nb.js
+++ 07_리팩토링 함수와 객체/00-nb.js
@@ -1,5 +1,5 @@
 // NBC (Naive Bayes Classifier)
-var classifier = {
+const classifier = {
   // 셋업 함수
   songs: [],
   allChords: new Set(),
@@ -7,6 +7,18 @@ var classifier = {
   labelProbabilities: new Map(),
   chordCountsInLabels: new Map(),
   probabilityOfChordsInLabels: new Map(),
+};
+
+// 노래 목록
+const songList = {
+  songs: [],
+  addSong(name, chords, difficulty) {
+    this.songs.push({
+      name,
+      chords,
+      difficulty,
+    });
+  },
 };
 
 // 현재 파일명 추출 함수
@@ -29,15 +41,59 @@ function setDifficulties() {
 
 // 노래 설정 함수
 function setSongs() {
-  imagine = ["c", "cmaj7", "f", "am", "dm", "g", "e7"];
-  somewhereOverTheRainbow = ["c", "em", "f", "g", "am"];
-  tooManyCooks = ["c", "g", "f"];
-  iWillFollowYouIntoTheDark = ["f", "dm", "bb", "c", "a", "bbm"];
-  babyOneMoreTime = ["cm", "g", "bb", "eb", "fm", "ab"];
-  creep = ["g", "gsus4", "c", "cmsus4", "cm6"];
-  paperBag = ["bm7", "e", "c", "g", "b7", "f", "em", "a", "cmaj7", "em7", "a7", "f7", "b"];
-  toxic = ["cm", "eb", "g", "cdim", "eb7", "d7", "db7", "ab", "gmaj7", "g7"];
-  bulletproof = ["d#m", "g#", "b", "f#", "g#m", "c#"];
+  songList.addSong(
+    "imagine",
+    ["c", "cmaj7", "f", "am", "dm", "g", "e7"],
+    easy,
+  );
+
+  songList.addSong(
+    "somewhereOverTheRainbow",
+    ["c", "em", "f", "g", "am"],
+    easy,
+  );
+
+  songList.addSong(
+    "tooManyCooks",
+    ["c", "g", "f"],
+    easy,
+  );
+
+  songList.addSong(
+    "iWillFollowYouIntoTheDark",
+    ["f", "dm", "bb", "c", "a", "bbm"],
+    medium,
+  );
+
+  songList.addSong(
+    "babyOneMoreTime",
+    ["cm", "g", "bb", "eb", "fm", "ab"],
+    medium,
+  );
+
+  songList.addSong(
+    "creep",
+    ["g", "gsus4", "c", "cmsus4", "cm6"],
+    medium,
+  );
+
+  songList.addSong(
+    "paperBag",
+    ["bm7", "e", "c", "g", "b7", "f", "em", "a", "cmaj7", "em7", "a7", "f7", "b"],
+    hard,
+  );
+
+  songList.addSong(
+    "toxic",
+    ["cm", "eb", "g", "cdim", "eb7", "d7", "db7", "ab", "gmaj7", "g7"],
+    hard,
+  );
+
+  songList.addSong(
+    "bulletproof",
+    ["d#m", "g#", "b", "f#", "g#m", "c#"],
+    hard,
+  );
 }
 
 function train(chords, label) {
@@ -98,18 +154,10 @@ function setLabelAndProbabilities() {
 function trainAll() {
   setDifficulties();
   setSongs();
-  
-  train(imagine, easy);
-  train(somewhereOverTheRainbow, easy);
-  train(tooManyCooks, easy);
-  
-  train(iWillFollowYouIntoTheDark, medium);
-  train(babyOneMoreTime, medium);
-  train(creep, medium);
-  
-  train(paperBag, hard);
-  train(toxic, hard);
-  train(bulletproof, hard);
+
+  songList.songs.forEach(song => {
+    train(song.chords, song.difficulty);
+  });
 
   setLabelAndProbabilities();
 }
